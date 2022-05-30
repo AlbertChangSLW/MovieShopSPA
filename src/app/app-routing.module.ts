7008a9b9dc182328959164358ab0fc37cea74cf2
YaoChung Chang <AlbertChangSLW@hotmail.com>
2022-05-30 11:24:02 -0600

    add the code from the 527 class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "movies", loadChildren: () => import("./public/movies.module").then(mod => mod.MoviesModule)},
+  { path: "user", loadChildren: () => import("./user/user.module").then(mod => mod.UserModule)},
+  { path: "account", loadChildren: () => import("./account/account.module").then(mod => mod.AccountModule)},
+  { path: "admin", loadChildren: () => import("./admin/admin.module").then(mod => mod.AdminModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
