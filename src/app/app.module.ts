ae834cd584286a906b5266d001cb405a5ade1b7d
YaoChung Chang <AlbertChangSLW@hotmail.com>
2022-06-02 14:22:47 -0600

    add the code from the 601 first part class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,6 +17,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     CoreModule,
     SharedModule
   ],
--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -1,17 +1,55 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { Login } from 'src/app/shared/Models/Login';
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { User } from 'src/app/shared/Models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  private currentUserSubject = new BehaviorSubject<any>({} as User);
+  public currentUser = this.currentUserSubject.asObservable();
 
-  login(){
+  private isLoggedInSubject = new BehaviorSubject<boolean>(false);
+  public isLoggedIn = this.isLoggedInSubject.asObservable();
 
+  private jwtHelper = new JwtHelperService();
+
+  constructor(private http: HttpClient) { }
+
+  login(login: Login): Observable<boolean>{
+    return this.http.post('https://localhost:7046/api/Account/login', login).pipe( map ( (response: any) => {
+    if(response){
+      localStorage.setItem('token', response.token);
+      this.populateUserInfoFromJwtToken();
+      return true;
+    }
+    return false;
+    }));
   }
 
   logout(){
-    
+    localStorage.removeItem('token');
+
+    this.currentUserSubject.next({} as User);
+    this.isLoggedInSubject.next(false);
+  }
+
+  populateUserInfoFromJwtToken() {
+
+    var tokenValue = localStorage.getItem('token');
+
+    if(tokenValue && !this.jwtHelper.isTokenExpired(tokenValue)){
+
+      const decodedToken = this.jwtHelper.decodeToken(tokenValue);
+
+      this.currentUserSubject.next(decodedToken);
+      this.isLoggedInSubject.next(true);
+
+    }
+
   }
 }
